699e7b5f3f73376648fbb42f515506630ce62f23
Rans4ckeR <ransacker@outlook.com>
2023-11-11 03:41:03 +0100

    Update build

--- BuildScripts/Build-Ares.ps1
+++ BuildScripts/Build-Ares.ps1
@@ -1,13 +1,13 @@
 #!/usr/bin/env pwsh
 #Requires -Version 7.2
 
-param($Configuration = "Release", $Framework = "net7.0")
+param($Configuration = "Release", $Framework = "net7.0", $AssemblySemVer = "0.0.0", $AssemblySemFileVer = "0.0.0.0", $InformationalVersion = "0.0.0-local")
 
 . $PSScriptRoot\Common.ps1
 
-Build-Project $Configuration Ares UniversalGL $Framework
+Build-Project $Configuration Ares UniversalGL $Framework $AssemblySemVer $AssemblySemFileVer $InformationalVersion
 if ($IsWindows) {
   @('WindowsDX', 'WindowsGL', 'WindowsXNA') | ForEach-Object {
-    Build-Project $Configuration Ares $_ $Framework'-windows'
+    Build-Project $Configuration Ares $_ $Framework'-windows' $AssemblySemVer $AssemblySemFileVer $InformationalVersion
   }
 }
--- BuildScripts/Build-TS.ps1
+++ BuildScripts/Build-TS.ps1
@@ -1,13 +1,13 @@
 #!/usr/bin/env pwsh
 #Requires -Version 7.2
 
-param($Configuration = "Release", $Framework = "net7.0")
+param($Configuration = "Release", $Framework = "net7.0", $AssemblySemVer = "0.0.0", $AssemblySemFileVer = "0.0.0.0", $InformationalVersion = "0.0.0-local")
 
 . $PSScriptRoot\Common.ps1
 
-Build-Project $Configuration TS UniversalGL $Framework
+Build-Project $Configuration TS UniversalGL $Framework $AssemblySemVer $AssemblySemFileVer $InformationalVersion
 if ($IsWindows) {
   @('WindowsDX', 'WindowsGL', 'WindowsXNA') | ForEach-Object {
-    Build-Project $Configuration TS $_ $Framework'-windows'
+    Build-Project $Configuration TS $_ $Framework'-windows' $AssemblySemVer $AssemblySemFileVer $InformationalVersion
   }
 }
